f51df0b2dce4f9d93734452e77e07d15d68e3b55
aboba <aboba@aboba.gg>
2022-12-05 16:41:29 +0500

    homework seminar1

---
+++ homework4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+int num2 = 2;
+while (num2 < num)
+{
+    Console.WriteLine(num2);
+    num2++;
+    num2++;
+}
+Console.WriteLine(num2);
